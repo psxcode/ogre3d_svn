8890e731b4ce25a8eb2662421b1a2a0c9324f72d
noamgat <noamgat@0896a727-e8d4-4d2b-8422-44d28760243c>
2008-06-28 14:28:28 +0000

    Added Isosurf skeleton to SVN

---
+++ Samples/Isosurf/src/Isosurf.cpp
@@ -0,0 +1,109 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2006 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+You may use this sample code for anything you like, it is not covered by the
+LGPL like the rest of the engine.
+-----------------------------------------------------------------------------
+*/
+/*
+-----------------------------------------------------------------------------
+Filename:    GeometryShader.cpp
+Description: Demo of a Shader Model 4 geometry shader in action
+-----------------------------------------------------------------------------
+*/
+
+#include "ExampleApplication.h"
+
+const String GLSL_MATERIAL_NAME = "Ogre/GPTest/SwizzleGLSL";
+const String ASM_MATERIAL_NAME = "Ogre/GPTest/SwizzleASM";
+const String CG_MATERIAL_NAME = "Ogre/GPTest/SwizzleCG";
+
+class GeometryShadingApplication : public ExampleApplication
+{
+public:
+    GeometryShadingApplication() { 
+    }
+
+    ~GeometryShadingApplication() {  }
+protected:
+
+    // Just override the mandatory create scene method
+    void createScene(void)
+    {
+        // Check capabilities
+		const RenderSystemCapabilities* caps = Root::getSingleton().getRenderSystem()->getCapabilities();
+        if (!caps->hasCapability(RSC_GEOMETRY_PROGRAM))
+        {
+			OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "Your card does not support geometry programs, so cannot "
+                "run this demo. Sorry!", 
+                "GeometryShading::createScene");
+        }
+		
+		int maxOutputVertices = caps->getGeometryProgramNumOutputVertices();
+		Ogre::LogManager::getSingleton().getDefaultLog()->stream() << 
+			"Num output vertices per geometry shader run : " << maxOutputVertices;
+
+        Entity *ent = mSceneMgr->createEntity("head", "ogrehead.mesh");
+        mCamera->setPosition(20, 0, 100);
+        mCamera->lookAt(0,0,0);
+		
+		String materialName = GLSL_MATERIAL_NAME;
+		//String materialName = ASM_MATERIAL_NAME;
+		//String materialName = CG_MATERIAL_NAME;
+
+		// Set all of the material's sub entities to use the new material
+		for (unsigned int i=0; i<ent->getNumSubEntities(); i++)
+		{
+			ent->getSubEntity(i)->setMaterialName(materialName);
+		}
+        
+        // Add entity to the root scene node
+        mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent);
+
+        mWindow->getViewport(0)->setBackgroundColour(ColourValue::Green);
+    }
+};
+
+
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+int main(int argc, char **argv)
+#endif
+{
+    // Create application object
+    GeometryShadingApplication app;
+
+    try {
+        app.go();
+    } catch( Exception& e ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+        std::cerr << "An exception has occured: " << e.getFullDescription();
+#endif
+    }
+
+
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
