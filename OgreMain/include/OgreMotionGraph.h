a31d3e4bea81fde5cdef994331a78d981bc267eb
westine <westine@0896a727-e8d4-4d2b-8422-44d28760243c>
2008-07-02 14:26:56 +0000

    Added OgreMotionGraph.h It contains classes MotionGraph and MotionGraphScript.

---
+++ OgreMain/include/OgreMotionGraph.h
@@ -0,0 +1,187 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2006 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+
+You may alternatively use this source under the terms of a specific version of
+the OGRE Unrestricted License provided you have obtained such a license from
+Torus Knot Software Ltd.
+-----------------------------------------------------------------------------
+*/
+#ifndef __MotionGraph_H__
+#define __MotionGraph_H__
+
+#include "OgrePrerequisites.h"
+#include "OgreAnimationState.h"
+#include <fstream>
+#include <deque>
+
+namespace Ogre {
+
+	/** A proxy class of Actor's behavior policy set .
+	@remarks
+	This class defines the interface an actor can access to determine how it Act(). 
+
+	Actor acts as the AI tells, commonly  via controlling the state of Actor's m_Entity and m_SceneNode,
+	especially is to set AnimationState.
+
+	@note
+	It is an abstract class, you should subclass it to create your instance class.
+
+	It is designed to utilize the following features:
+	Motion graphs and variants        
+	Motion interpolation and blending
+	Behavior-based graphs and search trees (similar to move trees)
+	Motion controllers learnt from existing data or based on physics 
+	Finite State Machine based AI
+
+	*/
+
+
+
+	class _OgreExport MotionGraph //: public AnimationStateSet
+	{
+	public:
+		/** A State is a character action unit, it keeps steady until some trigger pulses.
+		p.s. Trigger will be another class not created at this level of character animation.
+		@remarks
+		State can be reference to node in classical motion graph implementation,
+		but it extends that. State can have more character information, e.g. position,
+		orientation,velocity, acceleration,height and so on, if dynaimc is added, force will be
+		another varible of state.
+		@note
+		The differences of State in Ogre compared to other motion graph nodes
+		State's basic function is handling AnimationState in Ogre, animations will be
+		loaded in preprocess or on demand in unitive ResourceManager.
+
+		/************************************************************************/
+		class Transition;
+		/* 
+		Triggers are events that will cause a state transition.
+		*/
+		class Trigger 
+		{
+		public:
+			Trigger();
+			virtual ~Trigger();
+			typedef std::vector<Transition*> TransitionArray;
+		protected:
+			TransitionArray mTransitions;			
+		};
+
+		class State //: public AnimationState
+		{
+		public:
+			State(int stateid, const String& statename = ""):mStateID(stateid),mStateName(statename){};
+			virtual ~State(){};
+			typedef std::deque<Trigger*> TriggerQueue;
+			typedef std::map<float,Transition*> TransitionMap;
+			int GetStateID() const { return mStateID; }
+		protected:
+			String  mStateName;
+			int		mStateID;
+			TriggerQueue mTriggers; // it is a priority queue
+			TransitionMap mTransitions; // always select transition with the maximum probability,
+			// the probability is refreshed every time the a trigger is fired
+		};
+
+		class Transition
+		{
+		public:
+			Transition(){};
+			virtual ~Transition(){};
+			void AddFromState( State* pState) { mFromState = pState; }
+			void AddToState( State* pState ) { mToState = pState; }
+			void SetActionName( const String& actionName ) { mActionName = actionName; }
+		protected:
+			State* mFromState;
+			State* mToState;
+			String mActionName;
+
+		};
+
+		MotionGraph();
+		~MotionGraph();
+
+
+	};
+
+	/** A motion graph script is somewhat a Finite State Machine 
+	It defines a semantic as following
+	States:
+	1,[default action, accepted input triggers,...]
+	2,[default action, accepted input triggers,...]
+	3,[default action, accepted input triggers,...]
+	...
+	Transitions:
+	1,'AnimationEnd',walk,2
+	2,'AnimationEnd',run,3
+	3,'AnimationEnd',shot,1
+
+	The States section is a list of States.
+	The Transitions section is a Finite State Machine Table
+	CurrentState  InputTrigger   Action    NextState
+	    1         'turnleft'    turn left     2
+	@remarks
+	The first version of motion graph script is a native file that 
+	read directly using standardized fstream, once it works, a Resource
+	version of motion graph script is to be implemented
+	@note
+	Now this script is only for motion graph construction usage.
+	More functionalities are to be added to AI script.
+	And the script now is loaded when its parent skeleton is loaded synchronously,
+    
+	The final goal of using script is to integrate a script language virtual machine
+	into Ogre, e.g. Python, then adding new AI scripts and modifying exsiting scripts 
+	don't need to rebuild the source code of Ogre or its applications
+	/************************************************************************/
+
+	class _OgreExport MotionGraphScript //: public Resource
+	{
+	public:
+		MotionGraphScript(){};
+		virtual ~MotionGraphScript(){};
+		// this is an alpha version of script reader
+		bool Load(std::ifstream& ifMgScript);
+		typedef std::map<int,MotionGraph::State*> StateMap;
+		typedef std::vector<MotionGraph::Transition*> TransitionSet;
+	protected:
+		String mScriptName;
+		StateMap mStates;
+		TransitionSet mTransitions;
+
+		
+
+	};
+
+	enum MotionGraphScriptChunkID
+	{
+		MG_HEADER      =   0x1000,
+		MG_STATES      =   0x2000,
+			//////////////////////////////////////////////////////////////////////////
+			// repeated section of state
+			// 
+		MG_TRANSITIONS  =   0x3000
+	};
+
+}
+
+#endif
