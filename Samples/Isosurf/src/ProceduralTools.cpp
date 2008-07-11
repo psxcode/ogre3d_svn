966da9dc8af5455f6c0c649ebd61d2cf55ce5a6d
noamgat <noamgat@0896a727-e8d4-4d2b-8422-44d28760243c>
2008-07-11 13:44:12 +0000

    Fixed unsigned short overflow causing bad index buffer generation.

--- Samples/Isosurf/src/ProceduralTools.cpp
+++ Samples/Isosurf/src/ProceduralTools.cpp
@@ -50,11 +50,15 @@ void UnSwizzle(uint index, uint sizeLog2[3], uint * pPos)
 
 ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 {
+	//FILE* logFile = fopen("TetrahedraOgre.log","w");
+
 	ManualObject* tetrahedra = sceneManager->createManualObject("Tetrahedra");
 
 	uint sizeLog2[3] = { X_SIZE_LOG2, Y_SIZE_LOG2, Z_SIZE_LOG2 };
 	uint nTotalBits = sizeLog2[0] + sizeLog2[1] + sizeLog2[2];
 	uint nPointsTotal = 1 << nTotalBits;
+
+	//fprintf(logFile,"Num Points : %d\n" ,nPointsTotal);
 
 	tetrahedra->begin("Ogre/IsoSurf/TessellateTetrahedra");
 	tetrahedra->estimateVertexCount(nPointsTotal);
@@ -71,9 +75,11 @@ ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 		posVector.y = (Real(pos[1]) / Real(1<<Y_SIZE_LOG2))*2.0-1.0;
 		posVector.z = (Real(pos[2]) / Real(1<<Z_SIZE_LOG2))*2.0-1.0;
 		
+		//fprintf(logFile, "Point %d : %f %f %f\n", i, posVector[0], posVector[1], posVector[2]);
 		tetrahedra->position(posVector);
 	}
 
+	int quadNum = 0;
 	tetrahedra->estimateIndexCount(nPointsTotal * 6 * 4);
 	//Generate indices
 	for (uint i = 0; i<nPointsTotal; i++) {
@@ -93,19 +99,25 @@ ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 		// NOTE: order of vertices matters! important for backface culling
 
 		// T0
-		uint16 indices[4];
+		uint32 indices[4];
 		indices[0] = MAKE_INDEX(pos[0] + 1, pos[1], pos[2], sizeLog2);
 		indices[1] = MAKE_INDEX(pos[0], pos[1], pos[2], sizeLog2);
 		indices[2] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2], sizeLog2);
 		indices[3] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2] + 1, sizeLog2);
 		tetrahedra->quad(indices[0], indices[1], indices[2], indices[3]);
-
+//		fprintf(logFile, "Quad %d : %d, %d, %d, %d\n", quadNum++, indices[0], indices[1], indices[2], indices[3]);
+//#define QUAD_DEBUG_NUM -1
+//		assert(quadNum != QUAD_DEBUG_NUM);
+		
 		// T1
 		indices[0] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2] + 1, sizeLog2);
 		indices[1] = MAKE_INDEX(pos[0], pos[1], pos[2], sizeLog2);
 		indices[2] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2], sizeLog2);
 		indices[3] = MAKE_INDEX(pos[0], pos[1] + 1, pos[2], sizeLog2);
 		tetrahedra->quad(indices[0], indices[1], indices[2], indices[3]);
+		//fprintf(logFile, "Quad %d : %d, %d, %d, %d\n", quadNum++, indices[0], indices[1], indices[2], indices[3]);
+
+		//assert(quadNum != QUAD_DEBUG_NUM);
 
 		// T2
 		indices[0] = MAKE_INDEX(pos[0], pos[1] + 1, pos[2], sizeLog2);
@@ -113,6 +125,9 @@ ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 		indices[2] = MAKE_INDEX(pos[0], pos[1] + 1, pos[2] + 1, sizeLog2);
 		indices[3] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2] + 1, sizeLog2);
 		tetrahedra->quad(indices[0], indices[1], indices[2], indices[3]);
+		//fprintf(logFile, "Quad %d : %d, %d, %d, %d\n", quadNum++, indices[0], indices[1], indices[2], indices[3]);
+
+		//assert(quadNum != QUAD_DEBUG_NUM);
 
 		// T3
 		indices[0] = MAKE_INDEX(pos[0], pos[1], pos[2], sizeLog2);
@@ -120,6 +135,9 @@ ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 		indices[2] = MAKE_INDEX(pos[0], pos[1] + 1, pos[2] + 1, sizeLog2);
 		indices[3] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2] + 1, sizeLog2);
 		tetrahedra->quad(indices[0], indices[1], indices[2], indices[3]);
+		//fprintf(logFile, "Quad %d : %d, %d, %d, %d\n", quadNum++, indices[0], indices[1], indices[2], indices[3]);
+
+		//assert(quadNum != QUAD_DEBUG_NUM);
 
 		// T4
 		indices[0] = MAKE_INDEX(pos[0], pos[1], pos[2] + 1, sizeLog2);
@@ -127,6 +145,9 @@ ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 		indices[2] = MAKE_INDEX(pos[0] + 1, pos[1], pos[2] + 1, sizeLog2);
 		indices[3] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2] + 1, sizeLog2);
 		tetrahedra->quad(indices[0], indices[1], indices[2], indices[3]);
+		//fprintf(logFile, "Quad %d : %d, %d, %d, %d\n", quadNum++, indices[0], indices[1], indices[2], indices[3]);
+
+		//assert(quadNum != QUAD_DEBUG_NUM);
 
 		// T5
 		indices[0] = MAKE_INDEX(pos[0], pos[1], pos[2], sizeLog2);
@@ -134,8 +155,23 @@ ManualObject* ProceduralTools::generateTetrahedra(SceneManager* sceneManager)
 		indices[2] = MAKE_INDEX(pos[0] + 1, pos[1], pos[2] + 1, sizeLog2);
 		indices[3] = MAKE_INDEX(pos[0] + 1, pos[1] + 1, pos[2] + 1, sizeLog2);
 		tetrahedra->quad(indices[0], indices[1], indices[2], indices[3]);
+		//fprintf(logFile, "Quad %d : %d, %d, %d, %d\n", quadNum++, indices[0], indices[1], indices[2], indices[3]);
+
+		//assert(quadNum != QUAD_DEBUG_NUM);
 	}
 	
 	tetrahedra->end();
+
+	//Set the shader params.
+	/*int sizeMaskArray[] = { (1<<X_SIZE_LOG2)-1, (1<<Y_SIZE_LOG2)-1, (1<<Z_SIZE_LOG2)-1 };
+	int sizeShiftArray[] = { 0, X_SIZE_LOG2, X_SIZE_LOG2+Y_SIZE_LOG2 };
+	Ogre::Pass* targetPass = tetrahedra->getSection(0)->getMaterial()->getTechnique(0)->getPass(0);
+	targetPass->getVertexProgramParameters()->setNamedConstant("SizeMask", sizeMaskArray, 3);
+	targetPass->getVertexProgramParameters()->setNamedConstant("SizeShift", sizeShiftArray, 3);
+	targetPass->getGeometryProgramParameters()->setNamedConstant("SizeMask", sizeMaskArray, 3);
+	targetPass->getGeometryProgramParameters()->setNamedConstant("SizeShift", sizeShiftArray, 3);*/
+
+	//fclose(logFile);
+
 	return tetrahedra;
 }
